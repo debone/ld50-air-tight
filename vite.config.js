530b78e36846e35d2f73d30f9d4f4ecf73141533
Victor Debone <victor@debone.com.br>
2022-04-02 07:38:08 +0000

    Ready for dev

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,35 @@
-export default {
+import { fileURLToPath, URL } from "url";
+
+import { defineConfig } from "vite";
+
+let host;
+
+if (process.env.GITPOD_WORKSPACE_URL) {
+  console.log(
+    "Running from gitpod.io, making URLs from proxy",
+    process.env.GITPOD_WORKSPACE_URL
+  );
+  const { host: gitpodHost } = new URL(process.env.GITPOD_WORKSPACE_URL);
+  host = gitpodHost;
+} else {
+  host = "localhost";
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
   base: "./",
-};
+  define: {
+    __GITPOD_WORKSPACE_URL__: JSON.stringify(host),
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    port: 3000,
+    hmr: {
+      clientPort: 443,
+    },
+  },
+});
